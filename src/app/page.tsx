1f90825ab7dffeb85c478701f9432724c24ebec8
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-01 01:01:23 +0300

    Calling APIs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+interface searchParams {
+  genre:string
+}
+export default async function Home(searchParams:searchParams) {
+  const genre = searchParams.genre || 'fetchTrending'
+  const API_KEY = process.env.API_KEY
 
-export default function Home() {
-
+  const Response = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/movie/week/'}?language=en-US&page=1`,{
+    method:'GET',
+    headers:{
+      accept: 'application/json',
+    Authorization: `Bearer ${API_KEY} `
+    }
+  })
+  const data = await Response.json();
+  console.log(data)
   return <>
 
   <p>M11 by Memy11th</p>
