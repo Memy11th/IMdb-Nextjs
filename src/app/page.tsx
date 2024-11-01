3323f1f25283cb55c47a8eeb2bc5db35f0762411
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-01 17:54:20 +0300

    creating a server side generated component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
-interface searchParams {
-  genre:string
+
+interface SearchParams {
+  genre: string;
 }
-export default async function Home(searchParams:searchParams) {
-  const genre = searchParams.genre || 'fetchTrending'
-  const API_KEY = process.env.API_KEY
 
-  const Response = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/movie/week/'}?language=en-US&page=1`,{
-    method:'GET',
-    headers:{
-      accept: 'application/json',
-    Authorization: `Bearer ${API_KEY} `
+const API_KEY = process.env.API_KEY;
+
+export default async function Home(searchParams: SearchParams) {
+  const genre = searchParams.genre || 'fetchTrending';
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/${genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/movie/week'}?api_key=${API_KEY}&language=en-US&page=1`,
+      {
+        next: {
+          revalidate: 6000,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`API error: ${errorData.status_message}`);
     }
-  })
-  const data = await Response.json();
-  console.log(data)
-  return <>
 
-  <p>M11 by Memy11th</p>
-  </>
+    const {results} = await response.json();
+    console.log(results)
+  } catch (err) {
+    console.error('Fetch error:', err);
+    throw err;
+  }
+
+  return (
+    <div className="max-w-6xl mx-auto">
+      
+    </div>
+  );
 }
