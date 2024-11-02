44413932d62749c6e72f8b1e23b2222e7d493031
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-02 22:46:30 +0300

    adding some extra styles

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -9,10 +9,10 @@ export default function MovieCard({result}:ResultsPassedToMovieCard) {
         <h3 className='truncate font-semibold  '>{result.title}</h3>
         <p className='line-clamp-2 text-xs font-semibold  ' >{result.overview}</p>
         <div className='flex gap-3 mt-1 '>
-            <button className='p-2 bg-green-700 hover:bg-green-900 dark:bg-gray-800 dark:hover:bg-gray-900 rounded-lg w-2/3 transition-all duration-200 '>
+            <button className='p-2 bg-slate-500 hover:bg-slate-900 hover:text-white  dark:bg-gray-800 dark:hover:bg-gray-900 rounded-lg w-2/3 transition-all duration-200 '>
                 Add to watchlist
             </button>
-            <button className='p-2 bg-yellow-400 hover:bg-yellow-500 dark:bg-amber-500 dark:hover:bg-amber-600 transition-all duration-200 rounded-lg w-1/2 '>
+            <button className='p-2 bg-yellow-600 hover:bg-yellow-800 dark:bg-amber-500 dark:hover:bg-amber-600 transition-all duration-200 rounded-lg w-1/2 '>
                 Learn more
             </button>
         </div>
