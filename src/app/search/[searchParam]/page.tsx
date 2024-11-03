125989240d5b693365a1edb7ad72434e75c02557
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-03 22:01:52 +0300

    no movies found handle

--- src/app/search/[searchParam]/page.tsx
+++ src/app/search/[searchParam]/page.tsx
@@ -1,39 +1,37 @@
-import MovieCard from '@/components/MovieCard'
-import React from 'react'
+import MovieCard from '@/components/MovieCard';
+import React from 'react';
 import { Result } from "@/interfaces/resultsType";
 
-
 interface Params {
-    searchParam:string
+    searchParam: string;
 }
 
-interface PassedToSearchPage{
-    params:Params
+interface PassedToSearchPage {
+    params: Params;
 }
-const API_KEY = process.env.API_KEY
-export default async function page({params}:PassedToSearchPage) {
-    const search = params.searchParam
-    console.log(search)
 
-    const Response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}&language=en-US&include_adult=false`)
-    const data = await Response.json();
-    const movies = data?.results
-    console.log(movies)
+const API_KEY = process.env.API_KEY;
 
+export default async function Page({ params }: PassedToSearchPage) {
+    const search = params.searchParam;
+    console.log(search);
 
-    return <>
-    <div className="max-w-6xl mt-3 mx-auto grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-5 ">
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}&language=en-US&include_adult=false`);
+    const data = await response.json();
+    const movies = data?.results;
+    console.log(movies);
 
-        {
-        movies && movies.length === 0 (
-              <h4>No results found</h4> )
-               }
+    return (
+        <>
+            <div className="max-w-6xl mt-3 mx-auto grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-5">
+                {movies && movies.length === 0 && (
+                    <h4 className='align-middle '>No results found</h4>
+                )}
 
-    {movies?.map((movie:Result)=>{
-     return   <MovieCard key={movie.id} result={movie} />
-    })}
-
-    </div>
-    
-    </>
+                {movies?.map((movie: Result) => (
+                    <MovieCard key={movie.id} result={movie} />
+                ))}
+            </div>
+        </>
+    );
 }
